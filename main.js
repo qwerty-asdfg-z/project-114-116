227ec516cb0bcab8df5520ed5a84d0013b1f6980
qwerty-asdfg-z <75167037+qwerty-asdfg-z@users.noreply.github.com>
2022-01-30 13:31:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,37 @@
-    function preload(){
-
+noseX=0;
+noseY=0;
+moustache=""
+function preload(){
+    moustache=loadImage("https://i.postimg.cc/QtFGRRkY/imgbin-movember-moustache-mustache-csiz-Q6q-WQdd-NQZkt-PKPVuds-Dq-removebg-preview.png")
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.position(600,200);
+   video=createCapture(VIDEO)
+    video.size(300,300);
+    video.hide();
+   
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses)
+}
+function modelLoaded(){
+    console.log('PoseNet is initialised')
+}
+function draw(){
+image(video,0,0,300,300);
+   image(moustache,noseX-35,noseY,80,50) 
+    
+}
+function takeSnapshot(){
+    save("filter.jpg")
+}
+function gotPoses(results){
+    if (results.length>0)
+    {
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("nose x ="+noseX);
+        console.log("nose y ="+noseY);
     }
-    function setup(){
-        canvas=createCanvas(300,300);
-        canvas.center();
-       video=createCapture(VIDEO)
-        video.size(300,300);
-        video.hide();
-       
-        poseNet=ml5.poseNet(video, modelLoaded);
-        poseNet.on('pose',gotPoses)
-    }
-    function modelLoaded(){
-        console.log('PoseNet is initialised')
-    }
-    function draw(){
-    image(video,0,0,300,300);
-    }
-    function takeSnapshot(){
-        save("filter.jpg")
-    }
-    function gotPoses(results){
-        if (results.length>0)
-        {
-            console.log(results);
-            console.log("nose x ="+results[0].pose.nose.x);
-            console.log("nose y ="+results[0].pose.nose.y);
-        }
-
-
-
-
-    }
+}
